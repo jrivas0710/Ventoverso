0ca078bba597f7b68c8ec6d81fe6366fdf576f1b
berioska.monroy <berioska.monroy@gmail.com>
2023-08-29 00:26:16 -0400

    header listo, falta diseno

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,59 @@
 import { useState } from 'react'
 import './App.css'
 
-const nav = () => {
+export const Header= () => {
 
 
   return (
     <>
+    <div  className='grid-container'>
 
+
+  
+      <header className = 'header-grid'>
+
+        <section className='header-flex'>
+
+          <div className ='logo-header'>
+            Logo Ventoverso
+          </div>
+
+          <div>
+            <input type="text" />
+          </div>
+
+
+          <div>
+            icono
+            icono
+            icono
+          </div>
+
+        </section>
+
+        <section >
+          <ul className='categories-flex'>
+            <li>Bronces</li>
+            <li>Maderas</li>
+            <li>Herramientas</li>
+            <li>Accesorios</li>
+            <li>Ofertas</li>
+            <li>Ayuda</li>
+          </ul>
+        </section>
+
+
+      </header>
+      </div>
     </>
   )
-      
+
 }
 
-export default App
+
+
+
+
+
+
+
