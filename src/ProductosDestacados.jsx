3cee1e7fb23d1ae3775b0abf23abf1ab2d3e288e
berioska.monroy <berioska.monroy@gmail.com>
2023-08-30 18:23:08 -0400

    agregue seccion de productos destacados y sus detalles, me falta arreglar espacio de las estrelals

--- src/ProductosDestacados.jsx
+++ src/ProductosDestacados.jsx
@@ -4,21 +4,57 @@ export const Destacados = () => {
 
     return (
         <>
-          
 
 
-
-    <div className='productos-destacados'>
-
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
+            <div className='productos-destacados'>
 
 
 
 
-    </div>
-    </>)
+                <div>
+                    <div className='producto'></div>
+                    <div className='nombre-precio-ranking'>
+                        <img src="images/estrellas.png" alt="ranking" className='ranking' />
+                        <span className='nombre-producto'>Nombre del producto</span>
+                        <span className='precio'>$99.999</span>
+                    </div>
+                </div>
+
+
+                <div>
+                    <div className='producto'></div>
+                    <div className='nombre-precio-ranking'>
+                        <img src="images/estrellas.png" alt="ranking" className='ranking' />
+                        <span className='nombre-producto'>Nombre del producto</span>
+                        <span className='precio'>$99.999</span>
+                    </div>
+                </div>
+                <div>
+                    <div className='producto'></div>
+                    <div className='nombre-precio-ranking'>
+                        <img src="images/estrellas.png" alt="ranking" className='ranking' />
+                        <span className='nombre-producto'>Nombre del producto</span>
+                        <span className='precio'>$99.999</span>
+                    </div>
+                </div>
+                <div>
+                    <div className='producto'></div>
+                    <div className='nombre-precio-ranking'>
+                        <img src="images/estrellas.png" alt="ranking" className='ranking' />
+                        <span className='nombre-producto'>Nombre del producto</span>
+                        <span className='precio'>$99.999</span>
+                    </div>
+                </div>
+                <div>
+                    <div className='producto'></div>
+                    <div className='nombre-precio-ranking'>
+                        <img src="images/estrellas.png" alt="ranking" className='ranking' />
+                        <span className='nombre-producto'>Nombre del producto</span>
+                        <span className='precio'>$99.999</span>
+                    </div>
+                </div>
+
+
+            </div>
+        </>)
 }
