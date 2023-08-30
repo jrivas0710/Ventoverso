964e80feeb0e0047996a4535e6949c7e9417b2a4
berioska.monroy <berioska.monroy@gmail.com>
2023-08-30 16:18:22 -0400

    agregue seccion de productos destacados

---
+++ src/ProductosDestacados.jsx
@@ -0,0 +1,24 @@
+import './ProductosDestacados.css'
+
+export const Destacados = () => {
+
+    return (
+        <>
+          
+
+
+
+    <div className='productos-destacados'>
+
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+
+
+
+
+    </div>
+    </>)
+}
