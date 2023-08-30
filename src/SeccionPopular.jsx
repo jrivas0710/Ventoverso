d464e5828f22f0161e892e8f3a43b0da3eb4b2bd
berioska.monroy <berioska.monroy@gmail.com>
2023-08-30 11:43:48 -0400

    agregue componente seccionPopular.jsx

---
+++ src/SeccionPopular.jsx
@@ -0,0 +1,26 @@
+export const SeccionPopulares = () => {
+
+
+    return (
+      <>
+  
+        <section className='.grid-container'>
+  
+          <div className='seccion-populares'>
+  
+  
+            <div>hola</div>
+            <div>hola</div>
+            <div>hola</div>
+            <div>hola</div>
+  
+  
+  
+          </div>
+  
+        </section>
+  
+      </>
+    )
+  
+  }
