2f53905b926f15621a8e69b41e341ab7e5fc513c
berioska.monroy <berioska.monroy@gmail.com>
2023-08-31 09:09:38 -0400

    cree comoponente Iconos  hoja de estilo Iconos

---
+++ src/Iconos.jsx
@@ -0,0 +1,31 @@
+import './Iconos.css'
+
+
+
+export const Iconos = () => {
+
+
+    return (
+        <>
+
+
+            <section className='iconos'>
+
+
+
+
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+
+
+
+            </section>
+
+
+
+        </>
+    )
+
+}
