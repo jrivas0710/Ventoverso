709b2f3754c5515b4e4e842c60fa33717f1e21e7
berioska.monroy <berioska.monroy@gmail.com>
2023-08-31 09:01:59 -0400

    agregue texto de productos destacados

--- src/ProductosDestacados.jsx
+++ src/ProductosDestacados.jsx
@@ -9,9 +9,10 @@ export const Destacados = () => {
             <div className='productos-destacados'>
 
 
-
+            {/* todo esto sera luego un solo componente que se renderizara con map  */}
 
                 <div>
+                    <h2 className='texto-productos-destacados'>Productos Destacados</h2>
                     <div className='producto'></div>
                     <div className='nombre-precio-ranking'>
                         <img src="images/estrellas.png" alt="ranking" className='ranking' />
