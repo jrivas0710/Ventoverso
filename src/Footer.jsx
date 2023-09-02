169bd28bf1bfd72e8431d6a87b345385eb2a57a7
berioska.monroy <berioska.monroy@gmail.com>
2023-09-02 00:02:59 -0400

    todo listo para presentar el sabado

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -21,12 +21,12 @@ export const Footer = () => {
                     <p>Politicas de Privacidad</p>
                     <p>Termino y Condiciones</p>
                 </div>
-                <div className="footer3"><h3>Serivio al Cliente</h3>
+                <div className="footer3"><h3>Servicio al Cliente</h3>
                     <p>Centro de Ayuda</p>
                     <p>Seguimiento de Pedidos</p>
                     <p>Devoluciones y Reebolso</p>
                 </div>
-                <div className="footer4"><h3>Siguenos</h3>
+                <div className="footer4"><h3>Síguenos</h3>
                     <img src="/images/facebook.png" alt="enlace-facebook" className="facebook" />
                     <img src="/images/instagram.png" alt="enlace-instagram" className="instagram" />
                     <img src="/images/tik-tok.png" alt="enlace-tik-tok" className="tik-tok" />
