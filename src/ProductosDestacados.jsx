62929af5dbaac2448b49549d9102024b96365e9f
berioska.monroy <berioska.monroy@gmail.com>
2023-09-12 22:04:33 -0300

    cree mi esqueleto del componente producto destacado

--- src/ProductosDestacados.jsx
+++ src/ProductosDestacados.jsx
@@ -6,11 +6,11 @@ export const Destacados = () => {
         <>
 
 
-            <div className='productos-destacados'>
 
 
             {/* todo esto sera luego un solo componente que se renderizara con map  */}
 
+            <div className='productos-destacados'>
                 <div>
                     <h2 className='texto-productos-destacados'>Productos Destacados</h2>
                     <div className='producto'></div>
@@ -20,42 +20,11 @@ export const Destacados = () => {
                         <span className='precio'>$99.999</span>
                     </div>
                 </div>
-
-
-                <div>
-                    <div className='producto'></div>
-                    <div className='nombre-precio-ranking'>
-                        <img src="images/estrellas.png" alt="ranking" className='ranking' />
-                        <span className='nombre-producto'>Nombre del producto</span>
-                        <span className='precio'>$99.999</span>
-                    </div>
-                </div>
-                <div>
-                    <div className='producto'></div>
-                    <div className='nombre-precio-ranking'>
-                        <img src="images/estrellas.png" alt="ranking" className='ranking' />
-                        <span className='nombre-producto'>Nombre del producto</span>
-                        <span className='precio'>$99.999</span>
-                    </div>
-                </div>
-                <div>
-                    <div className='producto'></div>
-                    <div className='nombre-precio-ranking'>
-                        <img src="images/estrellas.png" alt="ranking" className='ranking' />
-                        <span className='nombre-producto'>Nombre del producto</span>
-                        <span className='precio'>$99.999</span>
-                    </div>
-                </div>
-                <div>
-                    <div className='producto'></div>
-                    <div className='nombre-precio-ranking'>
-                        <img src="images/estrellas.png" alt="ranking" className='ranking' />
-                        <span className='nombre-producto'>Nombre del producto</span>
-                        <span className='precio'>$99.999</span>
-                    </div>
-                </div>
-
-
             </div>
+
+
+                
+               
+
         </>)
 }
