71fed395e92bdae0d899a5f92da3097c9e8d721a
berioska.monroy <berioska.monroy@gmail.com>
2023-09-12 23:39:31 -0300

    proyecto convertido a typescript con exito

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import {Header} from './Header.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Header/>
     
